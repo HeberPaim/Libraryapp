4d937cf81eff8dd3e6df5293de5436061142d84e
Hereberz <heber.joan@gmail.com>
2024-04-14 19:24:53 -0300

    Removendo getters e setters tendo em vista que nenhum livro inicializado muda seu nume, nem autor e nem ano de publicacao. Apenas são lançadas novas edições.

--- src/main/java/org/example/Livro.java
+++ src/main/java/org/example/Livro.java
@@ -4,29 +4,9 @@ public class Livro {
     public String getTitulo() {
         return titulo;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public int getAnoDePublicacao() {
-        return anoDePublicacao;
-    }
-
-    public void setAnoDePublicacao(int anoDePublicacao) {
-        this.anoDePublicacao = anoDePublicacao;
-    }
-
-    private String titulo;
-    private String autor;
-    private int anoDePublicacao;
+    private final String titulo;
+    private final String autor;
+    private final int anoDePublicacao;
 
     public Livro(String titulo, String autor, int anoDePublicacao){
         this.titulo = titulo;
